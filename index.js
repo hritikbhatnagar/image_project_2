dd38d3f810ed34a22958b5e4f61c27cb7a9aa313
hritikbhatnagar <108444942+hritikbhatnagar@users.noreply.github.com>
2025-04-27 13:18:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connect_db from './db/db_connect.js';
+import router from './routes/get_image.route.js';
+import cors from 'cors';
+dotenv.config({path:"./.env"});
+const PORT=process.env.PORT || 8080;
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+connect_db();
+app.use(cors({origin:"http://127.0.0.1:3000",credentials:true}));
+app.use("/api/v1/user",router);
+
+
+app.listen(PORT,()=>
+{
+    console.log(`server is running at ${PORT}`);
+});
